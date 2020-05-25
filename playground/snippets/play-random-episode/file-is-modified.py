6ec249d1096b727b48078b75d7365a22636ca979
Rijin Mk <rijin.mk9@gmail.com>
2020-05-26 02:54:59 +0400

    Playground -> Snippet -> file-is-modified.py

---
+++ playground/snippets/play-random-episode/file-is-modified.py
@@ -0,0 +1,11 @@
+import time
+import os 
+
+command_file = "s.txt"
+
+h = [os.path.getmtime(command_file)]
+while True: 
+    time.sleep(1)
+    h.append(os.path.getmtime(command_file))
+    print(h[-1] - h[-2])
+
--- playground/snippets/play-random-episode/play-random-episode.py
+++ playground/snippets/play-random-episode/play-random-episode.py
@@ -1,8 +1,21 @@
 import io
+import time
+import os
 from textblob import TextBlob
 
-f = open("/Users/rijinmk/Dropbox/RED-MALUS/commands.txt", 'r')
-last = f.readlines()[-1].strip()
-f.close()
+command_file = "/Users/rijinmk/Dropbox/RED-MALUS/commands.txt"
+
+f_mod_o = os.path.getmtime(command_file)
+while True:
+    time.sleep(1)
+    f_mod_n = os.path.getmtime(command_file)
+    print("Time difference: ", f_mod_n - f_mod_o)
+    
+    f = open(command_file, 'r')
+    last = f.readlines()[-1].strip()
+    f.close()
+
+    last = TextBlob(last)
+    print(last.tags)
 
 print(last)
