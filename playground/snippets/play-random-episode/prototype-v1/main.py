998254514fb1e852d412f030e23e3e54dd4b5095
Rijin Mk <rijin.mk9@gmail.com>
2020-05-30 21:24:24 +0400

    Play Episode -> P1

--- playground/snippets/play-random-episode/prototype-v1/main.py
+++ playground/snippets/play-random-episode/prototype-v1/main.py
@@ -8,3 +8,4 @@ command_file = "/Users/rijinmk/Dropbox/RED-MALUS/commands.txt"
 base_url = "/Volumes/MAIN/uTorrent Zaid T.V./"
 
 # open "/Volumes/MAIN/uTorrent Zaid T.V./Friends/Season1/"
+# open "/Volumes/MAIN/uTorrent Zaid T.V./Friends/Season1/"
